405a97e125a363fe59ced200aee990c47784b16e
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-15 21:40:14 +0200

    Pass 2nd test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,17 @@
-const removeFromArray = function(items, item) {
+let newItems = "";
+
+const removeFromArray = function(items, item1, item2) {
     for (i=0; i<items.length; i++){
-        if (items[i] === item){
-            return items.toSpliced(i, 1); 
+        if (items[i] === item1){
+           newItems = items.toSpliced(i, 1);
         }
     }
-    
+    for (i=0; i<newItems.length; i++){
+        if (items[i] === item2){
+           newItems = newItems.toSpliced(i, 1);
+        }
+    }
+    return newItems;
 };
 
 // Do not edit below this line
