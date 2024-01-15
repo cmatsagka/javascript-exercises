ee2b51b2ef7b24759793b3ac21882e610f1b4f0b
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-16 00:17:31 +0200

    Pass 1st test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let leapYear = false;
 
+    if (year % 4 === 0){
+        leapYear = true;
+    }
+
+    return leapYear;
 };
 
 // Do not edit below this line
