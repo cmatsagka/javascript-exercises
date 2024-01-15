0949221fbfd72bfa7d1909de5a750cc6450d39b7
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-15 23:49:46 +0200

    Pass 5th test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,8 +2,12 @@ const sumAll = function(startRange, endRange) {
     let sum = 0;
     let min = Math.min(startRange, endRange);
     let max = Math.max(startRange, endRange);
+    let type1 = typeof startRange;
+    let type2 = typeof endRange;
 
     if (min < 0 || max < 0) {
+        return "ERROR";
+    }else if (type1 !== "number" || type2 !== "number") {
         return "ERROR";
     }else {
         for (i = min; i<= max; i++){
