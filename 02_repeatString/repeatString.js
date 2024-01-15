3561ba7a4c7e2878bd3a5f6bc9ef0d5eb0f0f529
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-15 16:23:10 +0200

    Pass first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    string = "hey";
+    num = 3;
+    let newString = "";
+    
+    for (i=0; i<num; i++){
+        newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
