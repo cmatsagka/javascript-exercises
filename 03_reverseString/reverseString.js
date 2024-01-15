e50d8c17b3f82cc15407b6e3038de815e64858f0
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-15 21:10:07 +0200

    Pass 1st test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+let wordLength = 0;
+let newWord = "";
 
+const reverseString = function(word) {
+    wordLength = word.length;
+
+    for (i = wordLength-1; i >= 0; i--) {
+        newWord += word[i];
+    }
+
+    return newWord;
 };
 
 // Do not edit below this line
