adab258601f21156326506082e55d6b2320808b8
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-29 16:01:09 +0200

    Pass not always true and single words

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[^\w\s\']|_/g, "")
+         .replace(/\s+/g, " ");
 
+    let strRev = str.split('').reverse().join('');
+    let arr = str.split('');
+
+    let result;
+
+    for (i = 0; i < arr.length; i++){
+        if (arr[i] !== strRev[i]){
+            result = false;
+            break;
+        }else{
+            result = true;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
