1d610cdd84270a95e93cbb30bb38e587dc4ac2db
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-29 21:29:46 +0200

    Pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    let a = 0;
+    let b = 1;
+    let preSum = a + b; //preSum = 1 a = b b = preSum
 
+    if (index > 0){
+        for (i = 1; i < index; i++){
+            preSum = a + b;
+            a = b;
+            b = preSum;
+        }
+    }else if (index == 0){
+        preSum = 0;
+    }else {
+        preSum = "OOPS";
+    }
+    
+    return preSum;
 };
 
 // Do not edit below this line
