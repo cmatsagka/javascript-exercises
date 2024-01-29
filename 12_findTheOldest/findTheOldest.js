bde92a1cba30eb3c3cfb81888e98ce3478132a49
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-29 22:39:47 +0200

    Pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age = 0;
+    let oldest = age;
+    let oldestPerson = "";
 
+    for (let person in people){
+        if (people[person].yearOfDeath === undefined){
+            const d = new Date();
+            people[person].yearOfDeath = d.getFullYear();
+        }
+        age = people[person].yearOfDeath - people[person].yearOfBirth;
+        
+        if (age > oldest){
+            oldest = age;
+            oldestPerson = people[person];
+        }
+    }
+    return oldestPerson;       
 };
 
 // Do not edit below this line
