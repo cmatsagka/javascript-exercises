10d4d674bf22f09899d528fc56cfd835397f590d
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2024-01-29 16:13:42 +0200

    Reduce code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,24 +1,8 @@
 const palindromes = function (str) {
-    str = str.toLowerCase();
-    str = str.replace(/[^\w\s\']|_/g, "")
-         .replace(/\s+/g, " ");
+    str = str.toLowerCase().replace(/[^\w\s\']|_/g, "")
+    .replace(/\s+/g, "");
 
-    str = str.split(' ').join('');
-
-    let strRev = str.split('').reverse().join('');
-    let arr = str.split('');
-
-    let result;
-
-    for (i = 0; i < arr.length; i++){
-        if (arr[i] !== strRev[i]){
-            result = false;
-            break;
-        }else{
-            result = true;
-        }
-    }
-    return result;
+    return str.split('').reverse().join('') == str;
 };
 
 // Do not edit below this line
